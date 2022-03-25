8855de8e18c8a45b9c0795c537acd6e003eb846d
Shir Avneri <shiravneri@gmail.com>
2022-03-25 17:44:12 +0300

    Petty changes

--- Main.py
+++ Main.py
@@ -1,29 +1,18 @@
 import Constants
 from PitchDetector import tuner
 
-def main():
-    target_note = input("please select note you which to tune to: E4, B3, G3, D3, A2, E2")
-    notes_dict = {}
-    notes_dict['E4'] = 329.6
-    notes_dict['B3'] = 246.9
-    notes_dict['G3'] = 196
-    notes_dict['D3'] = 143.8
-    notes_dict['A2'] = 110
-    notes_dict['E2'] = 82.4
-    target_pitch = notes_dict[target_note]
-
-    tuner()
-
-    current_pitch = max(set(Constants.PITCH_LIST), key=Constants.PITCH_LIST.count)
-    distance_to_pitch = target_pitch - current_pitch
-    print(f"you are {distance_to_pitch} away from your target!")
-
-
-
-
-
 
 if __name__ == "__main__":
-    main()
-
+    notes_dict = {'E4': 329.6, 'B3': 246.9, 'G3': 196, 'D3': 143.8, 'A2': 110, 'E2': 82.4}
+    while True:
+        target_note = input("Please select note you which to tune to: E4, B3, G3, D3, A2, E2\n").upper()
+        if target_note in notes_dict:
+            target_pitch = notes_dict[target_note]
+            break
+        else:
+            print("Illegal input, please try again...")
+    tuner()
+    current_pitch = max(set(Constants.PITCH_LIST), key=Constants.PITCH_LIST.count)
+    distance_to_pitch = target_pitch - current_pitch
+    print(f"You are {distance_to_pitch} away from your target!")
 
